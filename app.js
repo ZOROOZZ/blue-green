cc66250c3fb10e1c23a5af9462c75f5091491fd8
Mehul Saini <mehulsaini675@gamil.com>
2025-05-17 10:52:15 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// app.js
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+const VERSION = process.env.VERSION || 'v1 (Blue)';
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(`Hello from ${VERSION}!\n`);
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
